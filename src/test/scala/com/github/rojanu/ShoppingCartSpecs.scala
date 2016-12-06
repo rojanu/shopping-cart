9f6c054366767f1a6f23d9420296bbcc40d24180
rojanu <aliustek@gmail.com>
2016-12-06 22:37:29 +0000

    Add test case to total Apple, Apple, Orange, Apple

--- src/test/scala/com/github/rojanu/ShoppingCartSpecs.scala
+++ src/test/scala/com/github/rojanu/ShoppingCartSpecs.scala
@@ -11,4 +11,8 @@ class ShoppingCartSpecs extends FlatSpec with Matchers {
   "A shopping cart containing Apple, Orange" should "be charged £0.85 pence at checkout" in {
     new ShoppingCart(Seq(Apple, Orange)).total shouldBe 0.85
   }
+
+  "A shopping cart containing Apple, Apple, Orange, Apple" should "be charged £2.05 pence at checkout" in {
+    new ShoppingCart(Seq(Apple, Apple, Orange, Apple)).total shouldBe 2.05
+  }
 }
